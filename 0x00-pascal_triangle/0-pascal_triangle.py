eff4dc9c16d5e7abf81197e93a4c7c1a1e799a8e
saad484 <aiddisaad484@gmail.com>
2024-06-20 10:49:04 +0100

    pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Pascal Triangle
+"""
+
+
+def pascal_triangle(n) -> list:
+    """
+    pascal triangle O(n^2)
+    """
+
+    if n <= 0:
+        return res[[]]
+
+    res = [[1]]
+    for i in range(n-1):
+        temp = [0] + res[-1] + [0]
+        row = []
+        for j in range(len(res[-1]) + 1):
+            row.append(temp[j] + temp[j + 1])
+        res.append(row)
+    return res
