3a63a5245ac2592045b7ecd279b32aef3d9b5afd
saad484 <aiddisaad484@gmail.com>
2024-06-20 11:53:49 +0100

    unlock boxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Locked Boxes
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    Unlock Boxes
+    """
+    if not boxes:
+        return False
+
+    unlocked = set()
+    unlocked.add(0)
+    keys = list(boxes[0])
+    while keys:
+        key = keys.pop()
+        if key not in unlocked and key  < len(boxes):
+            unlocked.add(key)
+            keys.extend(boxes[key])
+    return len(unlocked) == len(boxes)
