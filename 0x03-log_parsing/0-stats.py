f0f379205375918ff1216d0767a996faeb118cf9
saad484 <aiddisaad484@gmail.com>
2024-06-24 00:27:09 +0100

    fixing one test case

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,49 +1,55 @@
 #!/usr/bin/python3
-'''
+"""
 Usage:
-    ./0-generator | ./0-stats.py
-'''
+    ./0-generator.py | ./0-stats.py
+"""
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import sys
 
-    status_counter = {
-            "200": 0,
-            "301": 0,
-            "400": 0,
-            "401": 0,
-            "403": 0,
-            "404": 0,
-            "405": 0,
-            "500": 0,
-            }
-
+    status_code_counts = {
+        "200": 0,
+        "301": 0,
+        "400": 0,
+        "401": 0,
+        "403": 0,
+        "404": 0,
+        "405": 0,
+        "500": 0,
+    }
     log_entry_count = 1
     file_size = 0
 
     def parse_line(log_entry):
         """
-        Read, prase, and extract relevant data from a given log entry.
+        Read, parse, and extract relevant data from a given log entry.
+
         Args:
-            log_entry (str): A log entry to be parsed
+            log_entry (str): A log entry to be parsed.
 
         Returns:
-            int: The siez of the file indicated in the log entry.
+            int: The size of the file indicated in the log entry.
         """
         try:
             parsed_entry = log_entry.split()
             status_code = parsed_entry[-2]
-            if status_code in status_counter.keys():
-                status_counter[status_code] += 1
-                return int(parsed_entry[-1])
+            if status_code in status_code_counts.keys():
+                status_code_counts[status_code] += 1
+            return int(parsed_entry[-1])
         except Exception:
             return 0
 
     def print_stats():
+        """
+        Print statistics in ascending order based on HTTP status codes.
+
+        Prints the total file size and the log_entry_count
+        of each HTTP status code encountered.
+        """
         print(f"File size: {file_size}")
-        for key in sorted(status_counter.keys()):
-            if status_counter[key]:
-                print(f"{key}: {status_counter[key]}")
+        for key in sorted(status_code_counts.keys()):
+            if status_code_counts[key]:
+                print(f"{key}: {status_code_counts[key]}")
 
     try:
         for log_entry in sys.stdin:
