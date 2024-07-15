8bb54d26f3f4d50af354d2c70b9cb2652a13534a
saad484 <aiddisaad484@gmail.com>
2024-07-15 13:00:48 +0100

    starwars characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,6 +2,7 @@
 const request = require('request');
 const API_URL = 'https://swapi-api.hbtn.io/api';
 
+
 if (process.argv.length > 2) {
 	request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
 		if (err) {
